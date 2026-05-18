d7ab45449171bccc8ed9883275de3982b9da63b0
afrin shaik <skafrin7013@gmail.com>
2026-05-18 22:22:46 +0530

    Variables remaing

--- src/Variable/Eg1.java
+++ src/Variable/Eg1.java
@@ -1,9 +1,13 @@
-package Variable;
+package com.practice.variables;
 
 public class Eg1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//variable in java is a container that holds the data which can be changed during the execution of a program
+		// dataType VariableName=value;
+		int age=23;
+		System.out.println("Age:" +age);
 
 	}
 
---
+++ src/Variable/Eg2.java
@@ -0,0 +1,19 @@
+package com.practice.variables;
+public class Eg2 {
+	public void showDetails()
+	{
+		int age=23;
+		//int age=23; here it is a local variable
+		//local variable is defined inside a method/block
+		//the scope of this variable is with in that block/method
+		System.out.println("Age:"+age);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.out.println(age);-> it an error"age cannot be resolved to a variable"
+		Eg2 a=new Eg2();
+		a.showDetails();
+	}
+
+}
+
