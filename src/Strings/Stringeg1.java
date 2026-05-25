2ff8f9f47096b2e5ba67bab4779751bb6cbacf8b
shaik <skafrin7013@gmail.com>
2026-05-25 17:17:04 +0530

    Strings

--- src/Strings/Stringeg1.java
+++ src/Strings/Stringeg1.java
@@ -1,11 +1,25 @@
 package Strings;
 
 public class Stringeg1 {
+	//string is the sequance of charcters in java
+	//strings are immutable in java means once declare we cannot change.
+	//we can create strings in java in 2 ways.
+	//string are stored in pool area when created with "",and store in heap area when created with "new" keyword.
 	
 	public static void main (String[] args)
 	{
-		String name=" Afrin";
-		System.out.println(name.length());
+		String s1="Afrin";
+		//using new keyword
+		
+		String s2 =new String("Learning");
+		String s3=new String("Java");
+		
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+		
+		
 	}
 
 }
---
+++ src/Strings/Stringeg2.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class Stringeg2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//length and character method
+		
+		//to find length of a string:
+		//using length() method
+		String s= "AfrinShaik";
+		System.out.println(s.length());
+		
+		//to character at particular index
+		System.out.println(s.charAt(7));
+
+		//to convert to char array we use charArray()
+		System.out.println(s.toCharArray());
+	}
+
+}
