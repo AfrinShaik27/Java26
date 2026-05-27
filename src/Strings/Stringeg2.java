9a7c522e3898502b22b4be831d37d3e1f2ff29ae
shaik <skafrin7013@gmail.com>
2026-05-27 23:49:17 +0530

    Patterns..

--- src/Strings/Stringeg2.java
+++ src/Strings/Stringeg2.java
@@ -16,6 +16,16 @@ public class Stringeg2 {
 
 		//to convert to char array we use charArray()
 		System.out.println(s.toCharArray());
+		
+		int count=0;
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
 	}
 
 }
