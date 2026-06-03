cf78f82027de70f60258f9bf876595ac064aa9bb
shaik <skafrin7013@gmail.com>
2026-06-03 23:54:09 +0530

    Strings...

---
+++ src/Strings/Eg1.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class Eg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Afrin";
+		name=name+" Shaik";
+		System.out.println("Hello "+name);
+		String s1="afrin";
+		String s2="afrin";
+		System.out.println(s1==s2);
+	}
+
+}
--- src/Strings/Stringeg2.java
+++ src/Strings/Stringeg2.java
@@ -14,18 +14,7 @@ public class Stringeg2 {
 		//to character at particular index
 		System.out.println(s.charAt(7));
 
-		//to convert to char array we use charArray()
-		System.out.println(s.toCharArray());
-		
-		int count=0;
-		for(int i=0;i<3;i++)
-		{
-			for(int j=0;j<3;j++)
-			{
-				count++;
-			}
-		}
-		System.out.println(count);
+	
 	}
 
 }
